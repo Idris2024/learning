7feea8a3d2b9c0a805d03060da600d2900ca597f
idris <navasarowolo@gmail.com>
2024-05-24 14:23:00 +0100

    ES6 Features

--- app2.js
+++ app2.js
@@ -1,1 +1,18 @@
-console.log("new file");
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// for (let i = 0; i < arr.length; i++) {
+//   const currentEl = arr[i];
+//   console.log('the current element is: ', currentEl);
+// }
+
+let i = 10;
+let j = 10;
+i--;
+j = j - 1;
+console.log('is i equals to j', i === j , i, j)
+ 
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16,17,18,19,20];
+ for (let i = 0; i < arr.length; i++) {
+   const currentEl = arr[i];
+   console.log('the current element is: ', currentEl);
+ } 
